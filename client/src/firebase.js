419e915b33d0e99cdb6378449bac3fa40d70d927
MaYnk0018 <126932719+MaYnk0018@users.noreply.github.com>
2024-06-17 11:48:16 +0530

    .env at client side

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5OVnqo0AUesKB-4fMz1qSKG0Qiv2814Y",
+  apiKey: import.meta.env.VITE_FIREBASE,
   authDomain: "mern-blog-e673f.firebaseapp.com",
   projectId: "mern-blog-e673f",
   storageBucket: "mern-blog-e673f.appspot.com",
