a480515d9cb4a9a5d4cf3ed50d4ba7abb5f97f21
MaYnk0018 <126932719+MaYnk0018@users.noreply.github.com>
2024-06-17 20:27:02 +0530

    dark theme+  header component extracting user info from redux

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export default function ThemeProvider({ children }) {
+  const {theme} = useSelector((state) => state.theme);
+  return <div className={theme}>{children}</div>;
+}
